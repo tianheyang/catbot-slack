8a98d848395e0879329f40a59c749926f023dd46
Tianhe Yang <tianhe.yang@gmail.com>
2016-05-17 17:49:17 -0700

    Converts app to bot

--- app.js
+++ app.js
@@ -1,28 +1,90 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var catbot = require('./catbot');
+require('dotenv').config();
+if (!process.env.BOT_TOKEN) {
+	console.log('Error: Specify token in environment');
+	process.exit(1);
+}
 
-var app = express();
-var port = process.env.PORT || 3000;
+// Variables
+var Botkit = require('botkit');
+var os = require('os');
+var rp = require('request-promise');
+var edgecatsService = {
+	uri: 'http://edgecats.net/random'
+};
+var catfactsService = {
+	uri: 'http://catfacts-api.appspot.com/api/facts'
+};
+var iconEmojis = [':smile_cat:',':joy_cat:',':smiley_cat:',':cat:'];
+var controller = Botkit.slackbot({
+	debug: true,
+});
+var bot = controller.spawn({
+	token: process.env.BOT_TOKEN
+}).startRTM();
 
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+// Helpers
+var randomElement = function(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+};
 
-// test route
-app.get('/', function (req, res) { res.status(200).send('<img src="http://edgecats.net/first" alt="a random cat gif!"/>') });
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+	bot.api.reactions.add({
+		timestamp: message.ts,
+		channel: message.channel,
+		name: 'cat',
+	}, function(err, res) {
+		if (err) {
+			bot.botkit.log('Failed to add emoji reaction :(', err);
+		}
+	});
 
-// get cat gif
-app.post('/getcat', catbot);
-
-// get cat facts
-app.get('/getfact', catbot);
-
-// error handler
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(400).send(err.message);
+	if (message.user && message.user.name) {
+		bot.reply(message, 'hai ' + user.name + '.');
+	} else {
+		bot.reply(message, 'o hai.');
+	}
 });
 
-app.listen(port, function () {
-  console.log('Slack bot listening on port ' + port);
+controller.hears(['gif','gifs','image','picture','gifme'], 'direct_message,direct_mention,mention', function(bot, message) {
+	var userName = message.user.name;
+	var catPromise = rp(edgecatsService);
+	var messages = ['check out dis kitteh.','ur gif r ready.','you can haz.','moar kittehz?'];
+	var goodbyes = ['', ' kthxbai.',' bai.'];
+	var botPayload = {
+		"username" : "catbot",
+		"response_type": "in_channel",
+		"icon_emoji" : randomElement(iconEmojis)
+	};
+
+	catPromise.then(function (catGif) {
+		// Process html...
+		botPayload.text = randomElement(messages) + randomElement(goodbyes);
+		botPayload.attachments = [{
+			"fallback": "Kitteh",
+			"image_url": catGif
+		}];
+		bot.reply(message, botPayload);
+	})
+	.catch(function (err) {
+		// Crawling failed...
+		bot.reply(message, "Couldn't get a cat gif right meow :(");
+	});
+});
+
+controller.hears(['fact','facts','factme'], 'direct_message,direct_mention,mention', function(bot, message) {
+	var factPromise = rp(catfactsService);
+	var botPayload = {
+		"username" : "catbot",
+		"response_type": "in_channel",
+		"icon_emoji" : ":information_source:"
+	};
+
+	factPromise.then(function (catFact) {
+		var factObject = JSON.parse(catFact);
+		botPayload.text = factObject.facts[0];
+		bot.reply(message, botPayload);
+	}).catch(function (err) {
+		// Crawling failed...
+		bot.reply(message, "Couldn't get a cat fact right meow :(");
+	});
 });
